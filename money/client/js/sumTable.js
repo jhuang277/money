f7c07417a746bbc6feee4b05799a70f33429fa5b
jhuang277 <jason@Jasons-MacBook-Pro.local>
2016-07-22 22:54:11 -0500

    templating

---
+++ money/client/js/sumTable.js
@@ -0,0 +1,10 @@
+// Transactions = new Mongo.Collection('transactions');
+// 
+// if(Meteor.isClient)
+//   Template.sumTable.helpers({
+//     transactions: [
+//       return Transactions.find();
+//     ]
+//   });
+//
+// }
